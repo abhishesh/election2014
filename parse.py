2932884c0c63f4da642dc8898dd0092c003b7319
Abhishesh <abhishesh@gmail.com>
2014-09-03 00:42:57 +0800

    Checking the final code written - way back

--- parse.py
+++ parse.py
@@ -2,39 +2,40 @@ import glob
 import json
 import pprint
 
-
 consolidated_list = []
-"""
-Read all htm files downloaded from election comission wesbite.
-Parse the details of winner from each seat.
-"""
-for item in glob.glob('./*.htm'):
+
+for item in glob.glob('data/*.htm'):
     temp = ""
-    temp_dict = {}
+    temp_lst = []
     try:
         with open(item, 'rU') as f:
             temp = f.readlines()[1145]
-            temp1 = temp.split('<')[3].split('>')[-1]
-            temp_dict['State'] = temp1.split('-')[0].strip()
-            temp_dict['Constitunecy'] = temp1.split('-')[-1].strip()
 
-            temp2 = temp.split('<')[19].split('>')[-1]
-            temp_dict['Candidate'] = temp2
+            temp_dict = {}
 
-            temp3 = temp.split('<')[21].split('>')[-1]
-            temp_dict['Party'] = temp3
+            data_str = [x.split('>')[-1] for x in temp.split('<')][3:-11]
 
-            temp4 = temp.split('<')[23].split('>')[-1]
-            temp_dict['Votes'] = int(temp4)
+            temp_dict['State'] = data_str[0].split('-')[0].strip()
+            temp_dict['Constituency'] = data_str[0].split('-')[-1].strip()
+
+            # Start Index
+            si = 16
+            ei = (len(data_str) if len(data_str) else 0)
+            temp_dict['Candidates'] = []
+            while si < ei:
+                candidate_dict = {}
+                candidate_dict['Candidate'] = data_str[si].strip()
+                candidate_dict['Party'] = data_str[si+2].strip()
+                candidate_dict['Votes'] = data_str[si+4].strip()
+                temp_dict['Candidates'].append(candidate_dict)
+                si = si + 8
+
+            temp_lst.append(temp_dict)
 
     except Exception as e:
         print "Exception:", e
 
-    consolidated_list.append(temp_dict)
+    consolidated_list.append(temp_lst)
 
-
-"""
-Dump the data in json file
-"""
 with open('data.json', 'wb') as fp:
-    json.dump(consolidated_list, fp)
+    json.dump(consolidated_list, fp, indent=4, sort_keys=True)
--- webscrapy.py
+++ webscrapy.py
@@ -1,44 +1,42 @@
 import urllib2
 
-"""
-Dictionary containing the State and UT codes
-as keys and the number of constituencies as values
-"""
-entities_dict = {"U01": 1,
-                 "S01": 42,
-                 "S02": 2,
-                 "S03": 14,
-                 "S04": 40,
-                 "U02": 1,
-                 "S26": 11,
-                 "U03": 1,
-                 "U04": 1,
-                 "S05": 2,
-                 "S06": 26,
-                 "S07": 10,
-                 "S08": 4,
-                 "S09": 6,
-                 "S27": 14,
-                 "S10": 28,
-                 "S11": 20,
-                 "U06": 1,
-                 "S12": 29,
-                 "S13": 48,
-                 "S14": 2,
-                 "S15": 2,
-                 "S16": 1,
-                 "S17": 1,
-                 "U05": 7,
-                 "S18": 21,
-                 "U07": 1,
-                 "S19": 13,
-                 "S20": 25,
-                 "S21": 1,
-                 "S22": 39,
-                 "S23": 2,
-                 "S24": 80,
-                 "S28": 5,
-                 "S25": 42}
+entities_dict = {
+                    "U01": 1,
+                    "S01": 42,
+                    "S02": 2,
+                    "S03": 14,
+                    "S04": 40,
+                    "U02": 1,
+                    "S26": 11,
+                    "U03": 1,
+                    "U04": 1,
+                    "S05": 2,
+                    "S06": 26,
+                    "S07": 10,
+                    "S08": 4,
+                    "S09": 6,
+                    "S27": 14,
+                    "S10": 28,
+                    "S11": 20,
+                    "U06": 1,
+                    "S12": 29,
+                    "S13": 48,
+                    "S14": 2,
+                    "S15": 2,
+                    "S16": 1,
+                    "S17": 1,
+                    "U05": 7,
+                    "S18": 21,
+                    "U07": 1,
+                    "S19": 13,
+                    "S20": 25,
+                    "S21": 1,
+                    "S22": 39,
+                    "S23": 2,
+                    "S24": 80,
+                    "S28": 5,
+                    "S25": 42,
+                }
 
 for item in entities_dict.keys():
     ctr = int(entities_dict[item])
@@ -46,8 +44,8 @@ for item in entities_dict.keys():
         filename = "Constituencywise" + item + str(i) + ".htm"
         try:
             data = urllib2.urlopen('http://eciresults.nic.in/%s?ac=%d'
-                                   % (filename, i))
-            with open('%s' % (filename), "w") as f:
+                                    % (filename, i))
+            with open('data/%s' % (filename), "w") as f:
                 temp = data.read()
                 f.write(temp)
         except Exception as e:
